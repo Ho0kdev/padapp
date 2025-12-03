5fb0184ee8f34127ff98f953555fd8110b51c015
Martin Petron <martinpetron@outlook.com>
2025-12-03 18:49:25 -0300

    fix: migration to react 19.2.1 and next 16.0.7

--- next.config.ts
+++ next.config.ts
@@ -9,10 +9,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Evita que el build falle por errores de ESLint (útil para despliegues en Dokploy, Vercel, etc.)
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     // Evita que el build falle por errores de typescript en producción (opcional)
     ignoreBuildErrors: false,
